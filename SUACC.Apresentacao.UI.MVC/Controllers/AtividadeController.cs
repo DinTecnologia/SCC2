547ae05cc2b52c25d874b196b7d9106947738979
dintecnologia <dintecnologia@outlook.com>
2018-02-20 19:00:20 -0300

    Alteração rotina

---
+++ SUACC.Apresentacao.UI.MVC/Controllers/AtividadeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SUACC.Apresentacao.UI.MVC.Controllers
+{
+    public class AtividadeController : Controller
+    {
+        // GET: Atividade
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
